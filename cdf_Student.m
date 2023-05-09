97b7b1877cd1dc2aab31ec8bc02a1c8f6e980ab0
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2023-05-09 15:53:48 +0200

    Update for Student distr w/ 5 or 8 df, n=5000

--- cdf_Student.m
+++ cdf_Student.m
@@ -1,16 +1,44 @@
-n = 40000;
-df_original = 5;
 
+%df      = [5 5 5 5 5];
+%lambda  = [1/5 1/5 1/5 1/5 1/5];
+
+n = 5000;
+df_original = 5;
 df = df_original * ones(n, 1);
 var = df_original / (df_original - 2);
-
 lambda = (1/sqrt(var * n)) * ones(n, 1);
 funtype = 1;
-x=(-5):0.1:5;
-
+x=(-5):0.01:5;
 [cdf,x] = tdist(x,df,lambda,funtype);
 
 plot(x,cdf);
 
-toWrite = [["x";x],["cdf";cdf]];
-writematrix(toWrite, "cdf_Student.csv");
+xstr = num2str(x,'%.3f');
+cdfstr = num2str(cdf,'%.15f');
+toWrite = [["x";xstr],["cdf";cdfstr]];
+
+fileName = append("cdf_Student_n", num2str(n, '%.0f'), "_", ...
+    num2str(df_original,'%.0f'), "df.csv");
+
+writematrix(toWrite, fileName);
+
+
+n = 5000;
+df_original = 8;
+df = df_original * ones(n, 1);
+var = df_original / (df_original - 2);
+lambda = (1/sqrt(var * n)) * ones(n, 1);
+funtype = 1;
+x=(-5):0.01:5;
+[cdf,x] = tdist(x,df,lambda,funtype);
+
+plot(x,cdf);
+
+xstr = num2str(x,'%.3f');
+cdfstr = num2str(cdf,'%.15f');
+toWrite = [["x";xstr],["cdf";cdfstr]];
+
+fileName = append("cdf_Student_n", num2str(n, '%.0f'), "_", ...
+    num2str(df_original,'%.0f'), "df.csv");
+
+writematrix(toWrite, fileName);
